2ab4728a6c3693849e057f771fcc80284a75677c
bhagathreddy123 <katta_reddy04@yahoo.com>
2023-12-28 05:30:52 -0800

    implementing copy to clipboard using jQuery

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_26_055313) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_28_103035) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_26_055313) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "buy_link"
   end
 
 end
