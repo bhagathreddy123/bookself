988e3d0f134af5ba0b52c9f7f4f9739df75bbd01
bhagathreddy123 <katta_reddy04@yahoo.com>
2023-12-29 01:04:26 -0800

    validations and faker gem for displaying seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+	validates :name, :email, :contact, :address, presence: true
+	validates :email, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,75 +7,88 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-books = Book.create([
-  { 
-    name: 'The Power of Positive Thinking',
-    price: 12.9,
-    author: 'Norman Vincent Peale',
-    category: 'Self Help',
-    buy_link: 'https://somebooksellingsite.com/purchase'
-  },
-  {
-    name: 'Atomic Habits',
-    price: 13.8,
-    author: 'James Clear',
-    category: 'Self Help',
-    buy_link: 'https://somebooksellingsite.com/purchase'
-  },
-  {
-    name: 'The Power of Habits',
-    price: 10.9,
-    author: 'Charles Duhigg',
-    category: 'Self Help',
-    buy_link: 'https://somebooksellingsite.com/purchase'
-  },
-  {
-    name: 'Attitude Is Everything: Change Your Attitude ... Change Your Life!',
-    price: 9.99,
-    author: 'Jeff Keller',
-    category: 'Self Help',
-    buy_link: 'https://somebooksellingsite.com/purchase'
-  },
-  {
-    name: 'You Can',
-    price: 4.0,
-    author: 'George Matthew Adams',
-    category: 'Self Help',
-    buy_link: 'https://somebooksellingsite.com/purchase'
-  },
-  {
-    name: 'The Power of Your Subconscious Mind',
-    price: 9.0,
-    author: 'Joseph Murphy',
-    category: 'Self Help',
-    buy_link: 'https://somebooksellingsite.com/purchase'
-  },
-  {
-    name: 'Commit to Yourself',
-    price: 9.75,
-    author: 'Nancy Katyal ',
-    category: 'Self Help',
-    buy_link: 'https://somebooksellingsite.com/purchase'
-  },
-  {
-    name: 'The Art of Focus: 45 Stories to Uplift the Mind and Transform the Heart',
-    price: 11.78,
-    author: 'Gauranga Das',
-    category: 'Self Help',
-    buy_link: 'https://somebooksellingsite.com/purchase'
-  },
-  {
-    name: 'Memory: How To Develop, Train, And Use It',
-    price: 13.9,
-    author: 'William Walker Atkinson',
-    category: 'Self Help',
-    buy_link: 'https://somebooksellingsite.com/purchase'
-  },
-  {
-    name: 'The Ultimate Guide To Success',
-    price: 12.99,
-    author: 'M.D. Julia Seton',
-    category: 'Self Help',
-    buy_link: 'https://somebooksellingsite.com/purchase'
-  }
-])
+# books = Book.create([
+#   { 
+#     name: 'The Power of Positive Thinking',
+#     price: 12.9,
+#     author: 'Norman Vincent Peale',
+#     category: 'Self Help',
+#     buy_link: 'https://somebooksellingsite.com/purchase'
+#   },
+#   {
+#     name: 'Atomic Habits',
+#     price: 13.8,
+#     author: 'James Clear',
+#     category: 'Self Help',
+#     buy_link: 'https://somebooksellingsite.com/purchase'
+#   },
+#   {
+#     name: 'The Power of Habits',
+#     price: 10.9,
+#     author: 'Charles Duhigg',
+#     category: 'Self Help',
+#     buy_link: 'https://somebooksellingsite.com/purchase'
+#   },
+#   {
+#     name: 'Attitude Is Everything: Change Your Attitude ... Change Your Life!',
+#     price: 9.99,
+#     author: 'Jeff Keller',
+#     category: 'Self Help',
+#     buy_link: 'https://somebooksellingsite.com/purchase'
+#   },
+#   {
+#     name: 'You Can',
+#     price: 4.0,
+#     author: 'George Matthew Adams',
+#     category: 'Self Help',
+#     buy_link: 'https://somebooksellingsite.com/purchase'
+#   },
+#   {
+#     name: 'The Power of Your Subconscious Mind',
+#     price: 9.0,
+#     author: 'Joseph Murphy',
+#     category: 'Self Help',
+#     buy_link: 'https://somebooksellingsite.com/purchase'
+#   },
+#   {
+#     name: 'Commit to Yourself',
+#     price: 9.75,
+#     author: 'Nancy Katyal ',
+#     category: 'Self Help',
+#     buy_link: 'https://somebooksellingsite.com/purchase'
+#   },
+#   {
+#     name: 'The Art of Focus: 45 Stories to Uplift the Mind and Transform the Heart',
+#     price: 11.78,
+#     author: 'Gauranga Das',
+#     category: 'Self Help',
+#     buy_link: 'https://somebooksellingsite.com/purchase'
+#   },
+#   {
+#     name: 'Memory: How To Develop, Train, And Use It',
+#     price: 13.9,
+#     author: 'William Walker Atkinson',
+#     category: 'Self Help',
+#     buy_link: 'https://somebooksellingsite.com/purchase'
+#   },
+#   {
+#     name: 'The Ultimate Guide To Success',
+#     price: 12.99,
+#     author: 'M.D. Julia Seton',
+#     category: 'Self Help',
+#     buy_link: 'https://somebooksellingsite.com/purchase'
+#   }
+# ])
+
+puts "creating Users"
+
+100.times do |i|
+ User.create(
+    name: Faker::Name.name_with_middle,
+    email: Faker::Internet.email,
+    address: Faker::Address.full_address,
+    contact: Faker::PhoneNumber::cell_phone_with_country_code
+  )
+
+ puts "User #{i+1} has been created"
+end
